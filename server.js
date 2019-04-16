e2357cb6b3fbad37b5a47911c8d6cf48a8dcc0ba
Shank <shank@Nomans-MacBook-Pro.local>
2019-04-15 22:50:22 -0500

    shank

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var path = require('path');
+var exphbs = require('express-handlebars');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
